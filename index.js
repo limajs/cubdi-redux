c1ec37074c6dd63d7b905dd24c90ce9862594836
simoncrabtree <simon@crabsticks.net>
2015-11-27 08:42:30 +0000

    Added all the code from the Redux counter example to get a working app

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import configureStore from './store/configureStore'
 
-console.log('Hello Redux')
+const store = configureStore()
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
